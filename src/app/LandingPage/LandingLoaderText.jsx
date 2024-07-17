8a7b365be8f4c0eefbc097d42e2e6c531cbcafa8
Bibaswan Nandi <94172148+Bibaswan03@users.noreply.github.com>
2024-07-18 01:07:16 +0530

    commit

--- src/app/LandingPage/LandingLoaderText.jsx
+++ src/app/LandingPage/LandingLoaderText.jsx
@@ -3,23 +3,23 @@ import './LandingLoaderText.css'; // Make sure to create and import the CSS file
 
 const BouncingText = () => {
     useEffect(() => {
-        const letters = document.querySelectorAll('.bounce');
+        const letters = document.querySelectorAll('.bounce12345');
         letters.forEach((letter, index) => {
             letter.classList.remove('hidden');
         });
     }, []);
 
     return (
-        <div className="container">
-            <span className="bounce">S</span>
-            <span className="bounce">w</span>
-            <span className="bounce">a</span>
-            <span className="bounce">g</span>
-            <span className="bounce">a</span>
-            <span className="bounce">t</span>
-            <span className="bounce">a</span>
-            <span className="bounce">m</span>
-            <span className="bounce">!</span>
+        <div className="container12345">
+            <span className="bounce12345">S</span>
+            <span className="bounce12345">w</span>
+            <span className="bounce12345">a</span>
+            <span className="bounce12345">g</span>
+            <span className="bounce12345">a</span>
+            <span className="bounce12345">t</span>
+            <span className="bounce12345">a</span>
+            <span className="bounce12345">m</span>
+            <span className="bounce12345">!</span>
         </div>
     );
 };
